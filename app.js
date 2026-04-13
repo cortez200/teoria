10f6edfc87b2de083e004e3128d58881d4146027
Adrian <adrian.cortez.tecsup.edu.pe>
2026-04-13 10:08:11 -0500

    Fix puerto para Render

--- app.js
+++ app.js
@@ -4,31 +4,28 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 
-
+// Motores
 app.set('view engine', 'pug');
-
 app.engine('hbs', require('hbs').__express);
 app.engine('ejs', require('ejs').renderFile);
 
 // RUTAS
 
-// PUG
 app.get('/pug', (req, res) => {
     res.render('pug/index');
 });
 
-// HBS
 app.get('/hbs', (req, res) => {
     res.render('hbs/index.hbs', { mensaje: "Bienvenidos a los motores de plantilla" });
 });
 
-// EJS
 app.get('/ejs', (req, res) => {
     res.render('ejs/index.ejs', { mensaje: "Bienvenidos a los motores de plantilla" });
 });
 
-const PORT = 3000;
+// 🔥 IMPORTANTE PARA RENDER
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
